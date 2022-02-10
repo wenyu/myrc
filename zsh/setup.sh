1e4d95a443b6eee94cb5fddf278fc692a132c5fd
Wenyu Zhang <hearson@gmail.com>
2022-02-10 15:24:57 -0800

    Add backup logic to zsh/setup.sh, making __WENYU_RC_ROOT a variable for other things.

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,10 +1,21 @@
 #!/bin/zsh
 set -ex
 SCRIPT_PATH="${0:a:h}"
-echo "source ${SCRIPT_PATH}/01_zshenv" >>$HOME/.zshenv
-echo "source ${SCRIPT_PATH}/02_zprofile" >>$HOME/.zprofile
-echo "source ${SCRIPT_PATH}/03_zshrc" >>$HOME/.zshrc
-echo "source ${SCRIPT_PATH}/04_zlogin" >>$HOME/.zlogin
-echo "source ${SCRIPT_PATH}/99_zlogout" >>$HOME/.zlogout
+CONFIG_FILES=( .zshenv .zprofile .zshrc .zlogin .zlogout )
 
+cd "$HOME"
+for config in $CONFIG_FILES; do
+    if [ -f $config ]; then
+        BACKUP_DIR=".zsh_backup.$(date +%s)"
+        mkdir $BACKUP_DIR
+        cp $CONFIG_FILES $BACKUP_DIR || true
+        break
+    fi
+done
 
+echo "__WENYU_RC_ROOT=\"${SCRIPT_PATH}\"" >>$HOME/.zshenv
+echo 'source ${__WENYU_RC_ROOT}/01_zshenv' >>$HOME/.zshenv
+echo 'source ${__WENYU_RC_ROOT}/02_zprofile' >>$HOME/.zprofile
+echo 'source ${__WENYU_RC_ROOT}/03_zshrc' >>$HOME/.zshrc
+echo 'source ${__WENYU_RC_ROOT}/04_zlogin' >>$HOME/.zlogin
+echo 'source ${__WENYU_RC_ROOT}/99_zlogout' >>$HOME/.zlogout
