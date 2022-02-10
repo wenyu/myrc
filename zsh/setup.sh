36e99ee0af4810bd26b91a413e051447ee06383d
Wenyu Zhang <hearson@gmail.com>
2022-02-10 15:46:00 -0800

    zsh/setup.sh allowing deletion of old config.

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,12 +3,24 @@ set -ex
 SCRIPT_PATH="${0:a:h}"
 CONFIG_FILES=( .zshenv .zprofile .zshrc .zlogin .zlogout )
 
+for arg in $(getopt 'r' $*); do
+    case "$arg" in
+        "-r")
+            echo "Will delete old configs after backup."
+            RESET="yes"
+            ;;
+    esac
+done
+
 cd "$HOME"
 for config in $CONFIG_FILES; do
     if [ -f $config ]; then
         BACKUP_DIR=".zsh_backup.$(date +%s)"
         mkdir $BACKUP_DIR
         cp $CONFIG_FILES $BACKUP_DIR || true
+        if [[ "$RESET" == "yes" ]]; then
+            rm $CONFIG_FILES || true
+        fi
         break
     fi
 done
